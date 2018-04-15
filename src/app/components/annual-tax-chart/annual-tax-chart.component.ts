07005351dacbac0e14dab5076e9ff7e6332726e5
IsuruNana <isurunanayakkara505@gmail.com>
2018-04-15 06:08:06 -0500

    Added pie graph

--- src/app/components/annual-tax-chart/annual-tax-chart.component.ts
+++ src/app/components/annual-tax-chart/annual-tax-chart.component.ts
@@ -14,15 +14,31 @@ import { Taxes } from '../../models/taxes';
 export class AnnualTaxChartComponent implements OnInit {
   //Declare data type
   pastTaxes:Taxes[];
-  specificTaxes:any[];
-  singleTax:Taxes;
+  baseYear:number;
+  currYear:number;
+  displayYear:number;
+
+  // Pie
+  public pieChartLabels:string[] = [
+    'defense',
+    'education',
+    'general',
+    'health_care',
+    'pensions',
+    'protection',
+    'transportation',
+    'welfare'
+  ];
+  public pieChartData:number[] = new Array<number>(8);
+  public pieChartType:string = 'pie';
 
   constructor(
     private pastTaxService: PastTaxesService,
     private calculatorService: CalculatorService
   ) { 
-    this.specificTaxes = new Array<Taxes>();
-    this.singleTax = new Taxes();
+    this.baseYear = 2000;
+    this.currYear = 0;
+    this.displayYear = this.baseYear + this.currYear;
   }
 
   ngOnInit() {
@@ -53,7 +69,7 @@ export class AnnualTaxChartComponent implements OnInit {
           .filter(key => allowed.includes(key))
           .map((obj, key) => {
             //console.log(taxes[i]);
-            return [obj, taxes[i][obj]];
+            return [obj, parseInt(taxes[i][obj])];
           });
 
         singleTaxObj.push(importantStuff);
@@ -75,7 +91,41 @@ export class AnnualTaxChartComponent implements OnInit {
 
   setPastTaxes(finalTaxes) {
     this.pastTaxes = finalTaxes;
+
+    this.updatePieChart();
     console.log(this.pastTaxes);
   }
 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+  public onYearButtonClicked(e:any): void {
+    this.currYear = (this.currYear + 1) % 18;
+    this.displayYear = this.baseYear + this.currYear;
+    this.updatePieChart();
+  }
+
+  public updatePieChart() {
+    let i = 0;
+    let j = 0;
+    let currArr;
+    let currArr2 = [];
+    for(i; i < 25; i++) {
+      if(this.displayYear.toString() == this.pastTaxes[i].year) {
+        for(j; j < 8; j++) {
+          currArr = this.pastTaxes[i].all_taxes_2[0][j][1];
+          console.log(currArr);
+          this.pieChartData[j] = parseInt(currArr);
+        }
+        break;
+      }
+    }
+    console.log(this.pieChartData);
+  }
 }
